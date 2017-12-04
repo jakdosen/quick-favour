8229f83d3790ae8d72d49749e7471885411ae8e4
Gavin Li <gavin@noosh.com>
2017-12-04 22:50:04 +0800

    PC 初始化

---
+++ build/pc/webpack.base.conf.js
@@ -0,0 +1,112 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('./config')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const glob = require('glob')
+
+function resolve (dir) {
+  return path.join(__dirname, '../../', dir)
+}
+
+let entries = getEntries(path.join(resolve('src/pc/scripts/modules') + '/**/*.js'));
+console.log(entries)
+let webpackConfig = {
+  context: path.resolve(__dirname, '../../'),
+  entry: entries,
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src/pc'),
+    }
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ],
+  },
+	plugins:[
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: Object.keys(entries).length
+		}),
+		new ExtractTextPlugin({
+			filename: utils.assetsPath('css/[name].css'),
+			allChunks: true,
+		}),
+	]
+}
+let htmls = getEntries(path.join(resolve('src/pc/views/') + '/**/*.html'));
+Object.keys(htmls).forEach(key =>{
+	webpackConfig.plugins.push(new HtmlWebpackPlugin({
+		template: htmls[key],
+		filename: 'views/' + key + '.html',
+		chunks: ['vendor', key],
+		inject: 'body',
+		hash:true,
+		minify: {
+			removeComments: true,
+			collapseWhitespace: true,
+			removeAttributeQuotes: true
+			// more options:
+			// https://github.com/kangax/html-minifier#options-quick-reference
+		},
+  }))
+})
+function getEntries(globPath) {
+	let files = glob.sync(globPath),
+		entries = {};
+
+	files.forEach(function(filepath) {
+
+    let parseObj = path.parse(filepath);
+		let basename = parseObj.name
+    if(basename == 'index'){
+			// 取倒数第二层(view下面的文件夹)做包名
+			basename = [parseObj.dir.split('/').slice(-1),basename].join('-')
+    }
+		entries[basename] = filepath;
+	});
+
+	return entries;
+}
+
+module.exports = webpackConfig
---
+++ src/pc/scripts/modules/login.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Gavin Li on 12/4/2017.
+ */
+import $ from 'jquery'
+import _ from 'underscore'
+import '@/styles/login.less'
+
+console.log($)
+console.log(_)
+console.log('login')
