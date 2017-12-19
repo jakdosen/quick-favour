5ea86e03fb37a21715ef995c9269f9a0344a90fc
shiyang.yao <562030859@qq.com>
2017-12-19 13:20:50 +0800

    支付成功页

---
+++ src/pc/scripts/modules/paySuccess.js
@@ -0,0 +1,9 @@
+/**
+ * Created by shiyang.yao on 2017/12/19.
+ */
+import '@/styles/paySuccess.less'
+import  sliderBar  from '@/scripts/common/sliderbar'
+import $ from 'jquery'
+import _ from 'underscore'
+
+new sliderBar();
