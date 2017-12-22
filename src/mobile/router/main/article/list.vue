689e16a77327223352efaa847112e1717b243460
gavin <liwirong@gmail.com>
2017-12-22 18:20:15 +0800

    添加收货地址  省 市 区 选择

--- src/mobile/router/main/article/list.vue
+++ src/mobile/router/main/article/list.vue
@@ -12,7 +12,7 @@
           </div>
           <div class="weui-media-box__bd">
             <h4 class="weui-media-box__title">{{item.title}}</h4>
-            <p class="weui-media-box__desc">{{item.desc}}</p>
+            <p class="weui-media-box__desc">&nbsp;<!--{{item.desc}}--></p>
             <p class="weui-media-box__tip"> {{item.like_num}}人秒赞</p>
           </div>
         </router-link>
