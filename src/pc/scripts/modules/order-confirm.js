9cc8f656c45402ab7267fd839d3fdac2ba5b1073
shiyang.yao <562030859@qq.com>
2017-12-29 18:23:59 +0800

    确认订单

--- src/pc/scripts/modules/order-confirm.js
+++ src/pc/scripts/modules/order-confirm.js
@@ -15,63 +15,6 @@ import {Model, Collection, View, Events} from 'backbone'
 let bus = window.bus;
 let moduleEv = _.extend({}, Events);
 
-let mockData = {
-  "goods_list": [
-    {
-      "cart_id": 1,
-      "goods_id": 430,
-      "goods_name": "以色列葡萄柚4个约250g/个",
-      "goods_sn": "MZ000430",
-      "cash_price": "30.00",
-      "coin_price": "40.00",
-      "goods_type": 3,
-      "goods_number": 1,
-      "goods_attr_id": "",
-      "goods_attr_str": "",
-      "is_checked": 1,
-      "is_on_sale": 1,
-      "goods_img": "http://t13.zetadata.com.cn/upload/images/201610/goods_img/430_G_1459971655294.jpg"
-    },
-    {
-      "cart_id": 14,
-      "goods_id": 460,
-      "goods_name": "菲律宾香蕉约1.5kg",
-      "goods_sn": "MZ000460",
-      "cash_price": "5.00",
-      "coin_price": "59995",
-      "goods_type": 2,
-      "goods_number": 2,
-      "goods_attr_id": "4044,4049",
-      "goods_attr_str": "越南 8成熟",
-      "is_checked": 1,
-      "is_on_sale": 1,
-      "goods_img": "http://t13.zetadata.com.cn/upload/images/201610/goods_img/460_G_1459126720606.jpg"
-    }
-  ],
-  "user_address": {
-    "id": 74,
-    "province": "云南省",
-    "city": "汕头市",
-    "county": "南苑乡",
-    "mobile": "13399545122",
-    "true_name": "哈士奇9",
-    "address": "哈哈哈哈哈哈就按上大街哈吉斯到行间阿斯蒂芬健康是福方式",
-    "is_default": 1
-  },
-  "user_account": {
-    "run_money": "0.00",
-    "m_coin": 0,
-    "can_use_run_money": "0.00",
-    "can_use_m_coin": 0
-  },
-  "cash_pay": {
-    "cash_total": 60.01
-  },
-  "cashcoin_pay": {
-    "cash_total": "40.00",
-    "coin_total": 120030
-  }
-};
 /**
  *  AddressPopupView
  */
@@ -115,12 +58,52 @@ let GoodsView = View.extend({
 });
 
 /**
+ * Address
+ */
+const Address = View.extend({
+  template: _.template($('#address-item').html()),
+  initialize() {
+    this.render();
+  },
+  render() {
+     this.$('.address-item').empty().append(this.template(this.model.toJSON()));
+  }
+});
+/**
+ * Account
+ */
+const Account = View.extend({
+  template: _.template($('#account').html()),
+  initialize() {
+    this.render();
+  },
+  render() {
+     this.$('.order-sub-body').empty().html(this.template(this.model.toJSON()));
+  }
+});
+
+/**
+ * 账户明细
+ */
+const PackageDetail = View.extend({
+  template: _.template($('#package').html()),
+  initialize() {
+    this.render();
+  },
+  render() {
+    this.$el.html(this.template(this.model.toJSON()));
+  }
+});
+
+/**
  * App
  */
 let App = View.extend({
   events: {
     'click .js-add-address-btn':'addAddress',
-    'click .check-btn': 'sendOrder'
+    'click .check-btn': 'sendOrder',
+    'click .order-type a':'changePayType',
+    'click .switch-wrap .switch':'changeSwitch'
   },
   initialize() {
     this.urlParams  = util.urlArgs();
@@ -128,12 +111,17 @@ let App = View.extend({
       bus.showLoginPopup();
       return this
     } else {
+      this.isAccount = 0;//是否使用账户余额 0表示不使用 1表示使用
+      this.type=0;// 0表示现金模式 1表示现金&秒币
       this.goodsList = new Collection();
       this.goodsList.bind('add', this.renderGoodsList, this);
       this.addressModel = new Model();
+      this.addressModel.bind('change', this.renderAddress, this);
       this.accountModel = new Model();
-      this.cashModel = new Model();
-      this.cashcoinModel = new Model();
+      this.accountModel.bind('change', this.renderAccount, this);
+      // 账单明细
+      this.accountList = new Model();
+      this.accountList.bind('change', this.renderPackage, this);
       this.fetchOrderData();
     }
   },
@@ -152,14 +140,12 @@ let App = View.extend({
         this.parseData(data);
       });
     }
-    this.parseData(mockData);
   },
   parseData(data){
     this.goodsList.add(data.goods_list);
     this.addressModel.set(data.user_address);
     this.accountModel.set(data.user_account);
-    this.cashModel.set(data.cash_pay);
-    this.cashcoinModel.set(data.cashcoin_pay);
+    this.accountList.set({...data,type:this.type,isAccount:this.isAccount});
   },
   renderGoodsList() {
     this.$('.js-goods-list-cot').empty();
@@ -168,6 +154,39 @@ let App = View.extend({
         model: cart
       }).$el.appendTo(this.$('.js-goods-list-cot'))
     })
+  },
+  renderAddress(){
+     this.address = new Address({
+       el:this.$('.order-address'),
+       model:this.addressModel
+     });
+  },
+  renderAccount(){
+     new Account({
+       el:this.$('.order-account'),
+       model:this.accountModel
+     });
+  },
+  renderPackage(){
+    this.packageDetail = new PackageDetail({
+      el:this.$('.order-packageDetail'),
+      model:this.accountList
+    });
+  },
+  changePayType(e){
+    let elem = this.$(e.currentTarget);
+    elem.addClass('checked').siblings().removeClass('checked');
+    this.accountList.set({type:elem.data('type')*1,isAccount:this.isAccount})
+  },
+  changeSwitch(e){
+    let elem = this.$(e.currentTarget);
+    if(elem.hasClass('switch-off')){
+      this.isAccount=1;
+      elem.addClass('switch-on').removeClass('switch-off');
+    }else{
+      this.isAccount=0;
+      elem.addClass('switch-off').removeClass('switch-on');
+    }
   },
   sendOrder(){
 
